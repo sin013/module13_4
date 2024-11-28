f02032c331f191849c8f6e64ed2c862545d183e3
sin013 <158771236+sin013@users.noreply.github.com>
2024-11-28 23:49:13 +0300

    Add files via upload

--- module13_4.py
+++ module13_4.py
@@ -1,54 +1,62 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
+
 import asyncio
 
-api = '***'
+api = ""
 bot = Bot(token=api)
-dp = Dispatcher(bot=bot, storage=MemoryStorage())
+dp = Dispatcher(bot, storage=MemoryStorage())
+s = 0
+
+
+@dp.message_handler(commands=["start"])
+async def start(message):
+    await message.answer("Привет! Я бот, помогающий твоему здоровью.")
+    s = 1
+    return s
+
+
+if s == 0:
+    @dp.message_handler()
+    async def all_massages(message):
+        await message.answer("Введите команду /start, чтобы начать общение.")
+
 
 class UserState(StatesGroup):
     age = State()
     growth = State()
     weight = State()
 
-@dp.message_handler(text=['Calories', 'Калории', 'Ккал'])
+
+@dp.message_handler(text="Calories")
 async def set_age(message):
-    await message.answer('Введите свой возраст:')
+    await message.answer("Введите свой возраст:")
     await UserState.age.set()
+
 
 @dp.message_handler(state=UserState.age)
 async def set_growth(message, state):
-    await state.update_data(age=message.text)
-    await message.answer('Введите свой рост (см):')
+    await state.update_data(ag=message.text)
+    await message.answer("Введите свой рост:")
     await UserState.growth.set()
+
 
 @dp.message_handler(state=UserState.growth)
 async def set_weight(message, state):
-    await state.update_data(growth=message.text)
-    await message.answer('Введите свой вес (кг):')
+    await state.update_data(grow=message.text)
+    await message.answer("Введите свой вес:")
     await UserState.weight.set()
+
 
 @dp.message_handler(state=UserState.weight)
 async def send_calories(message, state):
-    await state.update_data(weight=message.text)
+    await state.update_data(weig=message.text)
     data = await state.get_data()
-
-    try:
-        age = float(data['age'])
-        weight = float(data['weight'])
-        growth = float(data['growth'])
-    except:
-        await message.answer(f'Не могу конвертировать введенные значения в числа.')
-        await state.finish()
-        return
-
-    # Упрощенный вариант формулы Миффлина-Сан Жеора:
-    calories_man = 10 * weight + 6.25 * growth - 5 * age + 5
-    calories_wom = 10 * weight + 6.25 * growth - 5 * age - 161
-    await message.answer(f'Норма (муж.): {calories_man} ккал')
-    await message.answer(f'Норма (жен.): {calories_wom} ккал')
+    norma = int(10 * int(data['weig']) + 6.25 * int(data['grow']) - 5 * int(data['ag']) + 5)
+    await message.answer(f"Ваша норма в сутки {norma} ккал")
     await state.finish()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
